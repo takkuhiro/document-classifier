748573c6c5d4420c0fc933cb0d799043fbe45330
takkuhiro <n.hirotaka26@gmail.com>
2020-02-16 16:51:46 +0900

    13

--- ML/evaluate.py
+++ ML/evaluate.py
@@ -141,9 +141,6 @@ def evaluate_bert():
         idx = np.argmax(score)
         pred.append(idx)
         true.append(category_idx[ans])
-    assert len(pred) == len(true), 'len(pred): {},'
-                                   'len(true): {}'.format(len(pred),
-                                                          len(true))
 
     result = classification_report(true, pred)
     print(result)
--- ML/management/commands/train_bert.py
+++ ML/management/commands/train_bert.py
@@ -104,9 +104,6 @@ class Command(BaseCommand):
             idx = np.argmax(score)
             pred.append(idx)
             true.append(categories_idx[ans])
-        assert len(pred) == len(true), 'len(pred): {},'
-                                       'len(true): {}'.format(len(pred),
-                                                              len(true))
 
         result = classification_report(true, pred)
         print(result)
